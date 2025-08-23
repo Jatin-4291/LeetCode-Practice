cd433ee2ffef0f6ae3801a4580e5b4cd462604dc
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:40:28 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,30 @@
+// Last updated: 8/23/2025, 2:40:28 PM
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1){
+            return INT_MAX;
+        }
+        if (dividend == INT_MIN && divisor == 1){
+
+            return INT_MIN;
+        }
+        long long a = abs((long long)dividend);
+        long long b = abs((long long)divisor);
+        int result = 0;
+
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            a -= temp;
+            result += multiple;
+        }
+        if ((dividend < 0) ^ (divisor < 0))
+            result = -result;
+
+        return result;
+    }
+};
