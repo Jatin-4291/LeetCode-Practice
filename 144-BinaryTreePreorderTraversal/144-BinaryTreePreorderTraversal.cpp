41cfb2df7571d06a6ac655de2f576703cb7ad2bb
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:39:15 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/23/2025, 2:39:14 PM
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+
+            ans.push_back(node->val);
+            if (node->right) st.push(node->right);
+            if (node->left) st.push(node->left);
+        }
+
+        return ans;
+    }
+};
