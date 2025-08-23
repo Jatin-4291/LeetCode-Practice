e651e0fad8bcb2851d0c29d4d791d5976adffd31
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:40:49 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/23/2025, 2:40:49 PM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                ans.push_back(mp[complement]);
+                ans.push_back(i);
+                break;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
