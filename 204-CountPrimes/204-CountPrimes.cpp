0f4295a26c3bbd22940ffd39b1092861f2b348b9
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:38:50 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/23/2025, 2:38:50 PM
+class Solution {
+public:
+    int countPrimes(int n) {
+         if (n <= 2) return 0;
+        vector<int> isPrime(n,true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int i=2;i*i<n;i++){
+            if(isPrime[i]==true){
+                for(int j=i*i;j<n;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
