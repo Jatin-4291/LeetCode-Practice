14646ef59d47f45067086579d2f92dba52c617ff
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:37:53 +0530

    Create 543-DiameterOfBinaryTree

---
+++ 543-DiameterOfBinaryTree/543-DiameterOfBinaryTree.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/23/2025, 2:37:53 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root,int& ans){
+        if(root==nullptr) return 0;
+        int left=solve(root->left,ans);
+        int right=solve(root->right,ans);
+         ans=max(ans,left+right);
+         return 1+max(left,right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==nullptr) return 0;
+        int ans=0;
+        solve(root,ans);
+        return ans;
+    }
+};
