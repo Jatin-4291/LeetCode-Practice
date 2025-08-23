da59a44018d5249f64ea5546f08c5b1986c47ff3
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:36:38 +0530

    Create 2323-MinimumBitFlipsToConvertNumber

---
+++ 2323-MinimumBitFlipsToConvertNumber/2323-MinimumBitFlipsToConvertNumber.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/23/2025, 2:36:37 PM
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int x=start^goal;
+        int ans=0;
+        while(x>0){
+            x=(x&x-1);
+            ans++;
+        }
+        return ans;
+    }
+};
