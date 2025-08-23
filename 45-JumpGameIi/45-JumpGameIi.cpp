eb9f260638bb21141c9f5fa97d1b575de0fec114
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:40:17 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/23/2025, 2:40:17 PM
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return 0;
+
+        int jumps = 0;
+        int currEnd = 0;
+        int maxReach = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            maxReach = max(maxReach, i + nums[i]);
+            if (i == currEnd) {
+                jumps++;
+                currEnd = maxReach;
+            }
+        }
+        return jumps;
+    }
+};
