ede328c90ba0f5dd6fb2b9b4210bce6583a89227
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:37:39 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/23/2025, 2:37:39 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int h=nums.size()-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return -1;
+    }
+};
