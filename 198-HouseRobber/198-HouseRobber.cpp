1a99492597cc1f185c22b39d7a6e702d736d39ae
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:38:53 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/23/2025, 2:38:53 PM
+class Solution {
+public:
+    int solve(int i,vector<int>& nums, vector<int>& dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i]!=-1)return dp[i];
+        int rob=nums[i]+solve(i+2,nums,dp);
+        int skip=solve(i+1,nums,dp);
+        return dp[i]= max(rob,skip);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return solve(0,nums,dp);
+    }
+};
