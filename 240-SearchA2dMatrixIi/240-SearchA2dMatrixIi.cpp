ddebd68148d04538e81802305eebb4f12f06d927
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:38:34 +0530

    Create 240-SearchA2dMatrixIi

---
+++ 240-SearchA2dMatrixIi/240-SearchA2dMatrixIi.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/23/2025, 2:38:33 PM
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int i=0;
+        int j=m-1;
+        while(i<n && j>=0){
+            if(matrix[i][j]==target) return true;
+
+            else if(matrix[i][j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return false;
+        
+    }
+};
