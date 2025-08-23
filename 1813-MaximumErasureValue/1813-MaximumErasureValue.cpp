99a4141bc828a2ba01868fdefc9476d3c24b9d09
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:36:53 +0530

    Create 1813-MaximumErasureValue

---
+++ 1813-MaximumErasureValue/1813-MaximumErasureValue.cpp
@@ -0,0 +1,30 @@
+// Last updated: 8/23/2025, 2:36:52 PM
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> seen;
+        int left = 0;
+        int currentSum = 0;
+        int maxSum = 0;
+        
+        for (int right = 0; right < n; ++right) {
+            while (seen.find(nums[right]) != seen.end()) {
+                currentSum -= nums[left];
+                seen.erase(nums[left]);
+                ++left;
+            }
+            currentSum += nums[right];
+            seen.insert(nums[right]);
+            maxSum = max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+};
