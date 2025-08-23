f5730eff83053ac82b1cbe225213200720486e59
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-23 14:37:45 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/23/2025, 2:37:45 PM
+class Solution {
+public:
+
+    bool checkValidString(string s) {
+        int mini=0;
+        int maxi=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                mini++;
+                maxi++;
+            }
+            else if(s[i]==')'){
+                mini--;
+                maxi--;
+            }
+            else{
+                mini--;
+                maxi++;
+            }
+            if(mini<0) mini=0;
+            if(maxi<0) return false;
+        }
+        return mini==0;
+    }
+};
