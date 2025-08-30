d73d3e0fdc7689a85cf13a12c14b771cbb83d91d
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-30 13:22:37 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/30/2025, 1:22:34 PM
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        colors.insert(colors.end(), colors.begin(), colors.begin() + (k - 1));
+        int count = 0;
+        int left = 0;
+        for(int right = 0; right < colors.size(); ++right){
+            if(right>0&& colors[right]==colors[right-1]){
+                left=right;
+            }
+            if(right-left+1>=k){
+            count++;
+            }
+        }
+        return count;
+    }
+};
