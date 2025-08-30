26faba8830ee6837839172226a1343e530a863a5
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-30 13:24:42 +0530

    Create 2220-FindAllPossibleRecipesFromGivenSupplies

---
+++ 2220-FindAllPossibleRecipesFromGivenSupplies/2220-FindAllPossibleRecipesFromGivenSupplies.cpp
@@ -0,0 +1,60 @@
+// Last updated: 8/30/2025, 1:24:39 PM
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes,
+                                  vector<vector<string>>& ingredients,
+                                  vector<string>& supplies) {
+        vector<string> possibleRecipes;
+        // Track if ingredient/recipe can be made
+        unordered_map<string, bool> canMake;
+        // Map recipe name to its index in ingredients list
+        unordered_map<string, int> recipeToIndex;
+
+        // Mark all initial supplies as available
+        for (const string& supply : supplies) {
+            canMake[supply] = true;
+        }
+
+        // Create recipe to index mapping
+        for (int idx = 0; idx < recipes.size(); idx++) {
+            recipeToIndex[recipes[idx]] = idx;
+        }
+
+        // Try to make each recipe using DFS
+        for (const string& recipe : recipes) {
+            unordered_set<string> visited;
+            checkRecipe(recipe, ingredients, visited, canMake, recipeToIndex);
+            if (canMake[recipe]) {
+                possibleRecipes.push_back(recipe);
+            }
+        }
+
+        return possibleRecipes;
+    }
+
+private:
+    void checkRecipe(const string& recipe, vector<vector<string>>& ingredients,
+                     unordered_set<string>& visited,
+                     unordered_map<string, bool>& canMake,
+                     unordered_map<string, int>& recipeToIndex) {
+        if (canMake.find(recipe) != canMake.end()) {
+            return;
+        }
+        auto idxIt = recipeToIndex.find(recipe);
+        if (idxIt == recipeToIndex.end() || visited.count(recipe)) {
+            canMake[recipe] = false;
+            return;
+        }
+
+        visited.insert(recipe);
+        for (const string& ingredient : ingredients[idxIt->second]) {
+            checkRecipe(ingredient, ingredients, visited, canMake,
+                        recipeToIndex);
+            if (!canMake[ingredient]) {
+                canMake[recipe] = false;
+                return;
+            }
+        }
+        canMake[recipe] = true;
+    }
+};
