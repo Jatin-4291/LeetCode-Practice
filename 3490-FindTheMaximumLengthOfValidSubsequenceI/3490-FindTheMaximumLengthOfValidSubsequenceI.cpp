0fed7f1de67558a16f6950e283f8fb50bb3d6a5b
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-30 13:22:36 +0530

    Create 3490-FindTheMaximumLengthOfValidSubsequenceI

---
+++ 3490-FindTheMaximumLengthOfValidSubsequenceI/3490-FindTheMaximumLengthOfValidSubsequenceI.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/30/2025, 1:22:33 PM
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int cnt1 = 0, cnt2 = 0;
+        for (int it : nums) {
+            if (it % 2 == 0) cnt1++;
+            else cnt2++;
+        }
+
+        int eve = 0, odd = 0;
+        for (int it : nums) {
+            if (it % 2 == 0)
+                eve = max(eve, odd + 1);
+            else
+                odd = max(odd, eve + 1);
+        }
+
+        return max({cnt1, cnt2, eve, odd});
+    }
+};
