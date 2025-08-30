e3e593f1ce32a0f97aaadf0574059d2f680ad8dd
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-30 13:21:44 +0530

    Create 3778-TransformArrayByParity

---
+++ 3778-TransformArrayByParity/3778-TransformArrayByParity.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/30/2025, 1:21:41 PM
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=1;
+            }
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
