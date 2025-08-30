361dc8d5814b98a0db39a0e170b3b6dcdb2ede74
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-30 13:25:02 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/30/2025, 1:24:59 PM
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+     int i=0;
+     int j=0;
+     string ans="";
+     while(i<word1.size() || j<word2.size()){
+        if(i<word1.size()){
+        ans+=word1[i];
+        }
+        if(j<word2.size()){
+        ans+=word2[j];
+        }
+        i++;
+        j++;
+     }
+     return ans;   
+    }
+};
