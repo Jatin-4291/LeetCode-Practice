e981e68e59bce78b33a74fc858cc61cd06e0cdd2
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-08-30 13:22:08 +0530

    Create 3644-MinimumPositiveSumSubarray

---
+++ 3644-MinimumPositiveSumSubarray/3644-MinimumPositiveSumSubarray.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/30/2025, 1:22:06 PM
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+       int n = nums.size();
+        int minSum = INT_MAX;
+        bool found = false;
+
+        for (int k = l; k <= r; ++k) {
+            int sum = 0;
+            for (int i = 0; i < n; ++i) {
+                sum += nums[i];
+                if (i >= k - 1) {
+                    if (sum > 0) {
+                        minSum = min(minSum, sum);
+                        found = true;
+                    }
+                    sum -= nums[i - k + 1];
+                }
+            }
+        }
+
+        return found ? minSum : -1;
+    }
+};
