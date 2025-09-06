a0417468ad7144f4135a01b349979a8334a00412
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-09-06 11:08:30 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
@@ -0,0 +1,35 @@
+// Last updated: 9/6/2025, 11:08:29 AM
+class Solution {
+public:
+    int solve(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == nums.size()) return 0;
+        if (dp[i][prev + 1] != -1) return dp[i][prev + 1];
+
+        int take = 0;
+        if (prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + solve(i + 1, i, nums, dp);
+        }
+
+        int skip = solve(i + 1, prev, nums, dp);
+
+        return dp[i][prev + 1] = max(take, skip);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+        // return solve(0, -1, nums, dp);
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int take=0;
+                if(j==-1 || nums[i]>nums[j]){
+                    take=1+dp[i+1][i+1];
+                }
+                int skip=dp[i+1][j+1];
+                dp[i][j+1]=max(take,skip);
+            }
+        }
+        return dp[0][0];
+
+    }
+};
