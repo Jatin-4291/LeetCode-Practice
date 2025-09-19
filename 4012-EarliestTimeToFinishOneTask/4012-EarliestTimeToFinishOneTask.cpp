044d1d31ffd10d1a9d48fb60f8cd4e95dd2689c9
Jatin Dua <102898044+Jatin-4291@users.noreply.github.com>
2025-09-19 17:26:00 +0530

    Create 4012-EarliestTimeToFinishOneTask

---
+++ 4012-EarliestTimeToFinishOneTask/4012-EarliestTimeToFinishOneTask.cpp
@@ -0,0 +1,11 @@
+// Last updated: 9/19/2025, 5:26:00 PM
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int ans=INT_MAX;
+        for(auto task:tasks){
+            ans=min(ans,(task[0]+task[1]));
+        }
+        return ans;
+    }
+};
